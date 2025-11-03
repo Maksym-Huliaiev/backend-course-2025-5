9c49a6d771818bd2099f0dcfc8417c37287b08cc
Maksym-Huliaiev bc2024-5 <Maksym.Huliaiev@lnu.edu.ua>
2025-11-03 22:09:59 +0200

    part 2 method get

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const { Command } = require("commander");
 const fs = require("fs");
 const http = require("http");
+const { URL } = require("url");
+const path = require("path");
 
 const program = new Command();
 
 program
-  .requiredOption("-h, --host <host>", "адреса сервера")
-  .requiredOption("-p, --port <port>", "порт сервера")
-  .requiredOption("-c, --cache <path>", "шлях до директорії для кешування");
+    .requiredOption("-h, --host <host>", "адреса сервера")
+    .requiredOption("-p, --port <port>", "порт сервера")
+    .requiredOption("-c, --cache <path>", "шлях до директорії для кешування");
 
 program.parse(process.argv);
 const opts = program.opts();
@@ -15,12 +17,44 @@ const opts = program.opts();
 console.log(opts);
 
 if (!fs.existsSync(opts.cache)) {
-  fs.mkdirSync(opts.cache);
+    fs.mkdirSync(opts.cache);
 }
 
-const server = http.createServer((req, res) => {   
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello, World!\n");
+const server = http.createServer(async (req, res) => {
+    try {
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        console.log(`Received request: ${req.method} ${url.pathname}`);
+
+        const code = (url.pathname || "/").replace("/", "").trim();
+        console.log(`Requested code: ${code}`);
+
+        if (!code) {
+            res.writeHead(400, { "Content-Type": "text/plain" });
+            return res.end("Bad Request: expected path like /200");
+        }
+
+        const filePath = path.resolve(opts.cache, `${code}.jpg`);
+        console.log(`path: ${filePath}`);
+
+        if (req.method === "GET") {
+            try {
+                const data = await fs.promises.readFile(filePath);
+                res.writeHead(200, { "Content-Type": "image/jpeg" });
+                return res.end(data);
+            } catch (err) {
+                res.writeHead(404, { "Content-Type": "text/plain" });
+                return res.end("Not Found");
+            }
+        } else {
+            res.writeHead(405, { "Content-Type": "text/plain" });
+            return res.end("Method Not Allowed\n");
+        }
+
+    } catch (err) {
+        console.error(err);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        return res.end("Internal Server Error");
+    }
 });
 
 server.listen(opts.port, opts.host, () => {
